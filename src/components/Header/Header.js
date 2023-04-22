c59966d8f1973bf2488169e3fa92b607628cba77
Максим_Intern <110373927+Maksim20000@users.noreply.github.com>
2023-04-22 21:34:24 +1000

    NavBar-commit-refactor

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,47 +1,48 @@
-import {Button, Container, Form, FormControl, Nav, Navbar, NavbarBrand, NavLink} from "react-bootstrap";
-import logo from '../../assets/img/logo.png'
+import {Button, Container, Form, FormControl, Nav, Navbar, NavbarBrand, NavItem, NavLink} from "react-bootstrap";
+import logo from '../../assets/img/ReactIcon2.png'
 import NavbarToggle from "react-bootstrap/NavbarToggle";
 import NavbarCollapse from "react-bootstrap/NavbarCollapse";
 
 export let Header = () => {
-    return(
-        <div className={'justify-content-center'}>
-            {/*collapseOnSelect - делает navBar адаптивным*/}
-            {/*expand={} - брейк поинт при котором нав бар станет адаптивным*/}
-            {/*bg - стильnavbar*/}
-            {/*fixed={'top'} - делает что при скроле navBar не исчезает*/}
-            <Navbar fixed={'top'} collapseOnSelect expand={'sm'} bg={'dark'} variant={'dark'}>
+    return (
+        <div>
+            <Navbar className="navbar-expand-lg bg-body-tertiary"  fixed={'top'} collapseOnSelect expand={'md'} bg={'dark'} variant={'dark'}>
                 <Container>
-                    {/* В видио Navbar.brand*/}
-                    <NavbarBrand >
+                    <NavbarBrand>
                         <img src={logo}
                              alt="Logo"
-                            height={'30'}
-                             width={'30'}
                              className={'d-inline-block align-top'}
+                             height={50}
                         />
                     </NavbarBrand>
-                    {/* сделаю с документации*/}
-                    <NavbarToggle aria-controls="navbar-Supported-Content"/>
-
-                    {/*В документации id="navbar-Supported-Content"*/}
-                    <NavbarCollapse id="navbar-Supported-Content">
-                        <Nav className={'mr-auto'}>
-                            <NavLink src={'/'}>Home</NavLink>
-                            <NavLink src={'/about'}>About us</NavLink>
-                            <NavLink src={'/contacts'}>Contacts</NavLink>
-                            <NavLink src={'/blog'}>Blog</NavLink>
-                        </Nav>
-                        <Form  role="search" inline className={`d-flex`}>
-                            <FormControl
-                                type="search" placeholder="Search" aria-label="Search"
-                                className={'me-2'}
-                            />
-                            <Button variant={'outline-success'} className={'ms-lg-1'}>Search</Button>
-                        </Form>
-                    </NavbarCollapse>
-                    </Container>
+                        <NavbarToggle type="button" data-bs-toggle="collapse"
+                                      data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03"
+                                      aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </NavbarToggle>
+                        <NavbarCollapse id="navbarTogglerDemo03">
+                            <Nav className="me-auto mb-2 mb-lg-0">
+                                <NavItem>
+                                    <NavLink className=" active" aria-current="page" href="/">Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className=" active" aria-current="page" href="/about">About us</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className=" active" aria-current="page" href="/Contacts">Contacts</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className=" active" aria-current="page" href="/blog">Blog</NavLink>
+                                </NavItem>
+                            </Nav>
+                            <Form className="d-flex" role="search">
+                                <FormControl className="me-2" type="search" placeholder="Search"
+                                       aria-label="Search"/>
+                                <Button variant="outline-success" type="submit">Search</Button>
+                            </Form>
+                        </NavbarCollapse>
+                </Container>
             </Navbar>
         </div>
-    )
+        )
 }
